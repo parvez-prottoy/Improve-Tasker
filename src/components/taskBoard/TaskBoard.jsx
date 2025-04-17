d9e659ea59460627663d4b56a8f3f1d6272ea44a
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-04-17 20:07:34 +0600

    added task board all components design

--- src/components/taskBoard/TaskBoard.jsx
+++ src/components/taskBoard/TaskBoard.jsx
@@ -4,47 +4,15 @@ import TaskLists from "./TaskLists";
 
 export default function TaskBoard() {
   return (
-    <section class="mb-20">
+    <section className="mb-20">
       <div className="container">
         {/* Search Task */}
         <SearchTask />
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <div className="mb-14 items-center justify-between sm:flex">
-            <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
-            {/* TaskActions */}
-            <TaskActions />
-          </div>
-          <div className="overflow-auto">
-            <table className="table-fixed overflow-auto xl:w-full">
-              <thead>
-                <tr>
-                  <th className="p-4 pb-8 text-sm font-semibold capitalize w-[48px]"></th>
-                  <th className="p-4 pb-8 text-sm font-semibold capitalize w-[300px]">
-                    {" "}
-                    Title{" "}
-                  </th>
-                  <th className="p-4 pb-8 text-sm font-semibold capitalize w-full">
-                    {" "}
-                    Description{" "}
-                  </th>
-                  <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[350px]">
-                    {" "}
-                    Tags{" "}
-                  </th>
-                  <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">
-                    {" "}
-                    Priority{" "}
-                  </th>
-                  <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">
-                    {" "}
-                    Options{" "}
-                  </th>
-                </tr>
-              </thead>
-              {/* TaskLists */}
-              <TaskLists />
-            </table>
-          </div>
+          {/* TaskActions */}
+          <TaskActions />
+          {/* TaskLists */}
+          <TaskLists />
         </div>
       </div>
     </section>
--- src/components/taskBoard/TaskLists.jsx
+++ src/components/taskBoard/TaskLists.jsx
@@ -2,11 +2,38 @@ import TaskList from "./TaskList";
 
 export default function TaskLists() {
   return (
-    <tbody>
-      {/* TasList */}
-      <TaskList />
-      <TaskList />
-      <TaskList />
-    </tbody>
+    <div className="overflow-auto">
+      <table className="table-fixed overflow-auto xl:w-full">
+        <thead>
+          <tr>
+            <th className="p-4 pb-8 text-sm font-semibold capitalize w-[48px]"></th>
+            <th className="p-4 pb-8 text-sm font-semibold capitalize w-[300px]">
+              {" "}
+              Title{" "}
+            </th>
+            <th className="p-4 pb-8 text-sm font-semibold capitalize w-full">
+              {" "}
+              Description{" "}
+            </th>
+            <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[350px]">
+              {" "}
+              Tags{" "}
+            </th>
+            <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">
+              {" "}
+              Priority{" "}
+            </th>
+            <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">
+              {" "}
+              Options{" "}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* TaskList */}
+          <TaskList />
+        </tbody>
+      </table>
+    </div>
   );
 }
